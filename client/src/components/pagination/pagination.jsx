01f71918ea2bbb5041e50f5aaaca18946ba5be1b
jorge canale <jorgecanale636@yahoo.comm>
2022-10-25 13:40:13 -0300

    pagination with some style

--- client/src/components/pagination/pagination.jsx
+++ client/src/components/pagination/pagination.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
+import "./pagination.scss"
+
 
 export const Pagination  = ({page,setPage,max}) =>{
 
+    const [currentPage, setCurrentPage] = useState(1)
+
     const nextPage = ()=>{
+        setCurrentPage(currentPage + 1)
         setPage(page + 1)
+        window.scrollTo(0, 200)
     }
     const prevPage=()=>{
+        setCurrentPage(currentPage - 1)
         setPage(page - 1)
+        window.scrollTo(0, 200)
     }
 
+    const onKeyDown = e =>{
+        let val = parseInt(e.target.value)
+        console.log(val)
+        if(e.keyCode === 13){
+            setCurrentPage(parseInt(e.target.value))
+            if(isNaN(val) || val < 1 || val > max){
+                setCurrentPage(1)
+                setPage(1)
+            }else{
+                setPage(val)
+                window.scrollTo(0, 200)
+            }
+        }
+    }
 
     return(
-        <div>
-            <button onClick={prevPage} disabled={page === 1}>
+        <div className="pagination">
+            <button className="previous" onClick={prevPage} disabled={page === 1}>
                 prev
             </button>
-            {}
-            <button onClick={nextPage} disabled={ page === max}>
+            <input className="page" value={currentPage} autoComplete="off" onKeyDown={e=>onKeyDown(e)} onChange={e=>setCurrentPage(e.target.value)}/>
+             <p> of {max}</p>
+            <button className="next" onClick={nextPage} disabled={ page === max}>
                 next  
             </button>
         </div>
