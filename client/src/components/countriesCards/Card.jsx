f424903d1665038cdb61b5841a0cf8fbd751448c
jorge canale <jorgecanale636@yahoo.comm>
2022-10-27 09:27:34 -0300

    a little change for clean de country state

--- client/src/components/countriesCards/Card.jsx
+++ client/src/components/countriesCards/Card.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import {useNavigate} from "react-router-dom"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { countryByName } from "../../redux/actions/countriesActions";
+import { cleanErrorState } from "../../redux/actions/countriesActions";
 import './Card.scss'
 
 
 export const Card = ({name, flag, continent,languages, population,cid}) =>{
 
     let countryLanguages = languages !== undefined && languages !== null ? [] : null
-
+    const test = useSelector(store=>store.countries.Country)
     if(countryLanguages !== null ) for(let key in languages) countryLanguages.push(languages[key])
 
     const navigate = useNavigate()
@@ -18,13 +19,18 @@ export const Card = ({name, flag, continent,languages, population,cid}) =>{
         navigate(`/detail/${cid}`)
     }
 
+    const handleMouseLeave=()=>{
+        dispatch(cleanErrorState())
+        console.log(test)
+    }
+
     const handlerMouseOver =()=>{
         dispatch(countryByName(name))
     }
 
     return(
         <div className="cardContainer">
-            <div className="Card" onMouseOver={handlerMouseOver} onClick={handleClick}>
+            <div className="Card" onMouseOver={handlerMouseOver} onMouseLeave={handleMouseLeave} onClick={handleClick}>
                 <img className="flag" src={flag} alt='flag not found'/>
                 <h3 className="name">{name}</h3>
                 <p className="continet">{`Continent: ${continent}`}</p>
